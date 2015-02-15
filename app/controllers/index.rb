873cb516c26dd50d87f9d6e7686fc2984ffadd2c
Rhoxio <rhoxiodbc@gmail.com>
2015-02-14 17:24:21 -0800

    Set up the back end for API usage. Got the post routes working well. Just needs templating now.

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,3 +1,42 @@
+before do
+	content_type :json
+	cors_set_access_control_headers
+end
+
+set :protection, false
+
+options '/characters' do
+    200
+end
+
 get '/' do 
 	erb :index
+end
+
+get '/character/:id' do
+	@character = Character.find(params[:id])
+	@character.to_json
+end
+
+get '/characters' do
+	@characters = Character.all
+	@characters.to_json
+end
+
+post '/characters' do
+
+	begin
+		params.merge! JSON.parse(request.env["rack.input"].read)
+	rescue JSON::ParserError
+		logger.error "Cannot parse response body."
+	end
+
+	@new_character = Character.new(name: params[:name], player_class: params[:player_class])
+	if @new_character.save
+		puts 'Success!'
+		{result: params[:name], seen:true}.to_json
+	else
+		puts "Failed to create character"
+	end
+	
 end
---
+++ db/seeds.rb
@@ -0,0 +1,17 @@
+Character.create!(name: "Azorius", player_class: "Paladin", gender: "Male", currency: 50, exp: 0, level: 1, max_hp: 50, current_hp: 50, skill: 1, skill_exp: 0, attack_rating: 1, initiative: 1, defense: 1)
+Character.create!(name: "Rhoxio", player_class: "Paladin", gender: "Male", currency: 50, exp: 0, level: 1, max_hp: 50, current_hp: 50, skill: 1, skill_exp: 0, attack_rating: 1, initiative: 1, defense: 1)
+Character.create!(name: "Judee", player_class: "Paladin", gender: "Male", currency: 50, exp: 0, level: 1, max_hp: 50, current_hp: 50, skill: 1, skill_exp: 0, attack_rating: 1, initiative: 1, defense: 1)
+Character.create!(name: "Tsurung", player_class: "Paladin", gender: "Male", currency: 50, exp: 0, level: 1, max_hp: 50, current_hp: 50, skill: 1, skill_exp: 0, attack_rating: 1, initiative: 1, defense: 1)
+Character.create!(name: "Squirts", player_class: "Paladin", gender: "Male", currency: 50, exp: 0, level: 1, max_hp: 50, current_hp: 50, skill: 1, skill_exp: 0, attack_rating: 1, initiative: 1, defense: 1)
+
+
+
+
+Enemy.create!(name: "Rat", enemy_class: "beast", level: 1, exp: 10, max_hp: 10, current_hp: 10, skill: 1, attack_rating: 1, initiative: 1, defense: 1, currency: 2)
+
+User.create!(name: "Kevin", email:"rhoxiodbc@gmail.com", password_hash: "password", current_character: 1)
+
+
+
+
+
